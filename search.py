49a18e456a7a788ecd0906b69ecab0a359421db9
jeannecoleongco <coleongc@ualberta.ca>
2021-05-02 20:39:48 -0600

    database and program

--- search.py
+++ search.py
@@ -39,6 +39,7 @@ def load_searchdb():
 								header[8]: int(row[8]) # in_stock
 								}
 			searchdb.search_images.insert_one(search_document)
+			print(search_document)
 
 	# index the text information (name, description, site_tags, seller_tags)
 	searchdb.search_images.create_index([("text_info", TEXT)])
@@ -47,16 +48,21 @@ def load_searchdb():
 def search_by_image(img_id):
 	global searchdb
 	query = searchdb.search_images.find_one({ 'img_id': int(img_id) })
-	keywords = query['text_info']
-	file_type = query['file_type'].split()
-	rating = query['buyer_rating']
-	price = query['price']
-	in_stock = query['in_stock']
-	search(in_stock, price, rating, file_type, keywords)
+	if query:
+		keywords = query['text_info']
+		file_type = query['file_type'].split()
+		rating = query['buyer_rating']
+		price = query['price']
+		in_stock = query['in_stock']
+		search(in_stock, price, rating, file_type, keywords)
+	else:
+		print("\nImage does not exist.")
 
 
 def search(in_stock, price, rating, file_type, keywords):
 	global searchdb
+	all_results = set()
+
 	# search from text/characteristics
 	keyword_query = searchdb.search_images.find({ "$text": {"$search": keywords} }, 
 												{ 'score': { "$meta": "textScore" } }).sort([('score', {'$meta': 'textScore'})])
@@ -76,6 +82,7 @@ def search(in_stock, price, rating, file_type, keywords):
 												  { 'img_id': True, '_id': False })
 		for p in price_query:
 			price_img_ids.append(p['img_id'])
+			all_results.add(p['img_id'])
 
 	# in stock query
 	stock_img_ids = []
@@ -84,6 +91,7 @@ def search(in_stock, price, rating, file_type, keywords):
 												  { 'img_id': True, '_id': False })
 		for s in stock_query:
 			stock_img_ids.append(s['img_id'])
+			all_results.add(s['img_id'])
 
 	# img_ids are the foreign key into imagesdb, which contains the actual images to display
 	# this is the only info we need as a result of the search
@@ -93,59 +101,75 @@ def search(in_stock, price, rating, file_type, keywords):
 	
 	for r in rating_query:
 		rating_img_ids.append(r['img_id'])
+		all_results.add(r['img_id'])
 	for f in file_type_query:
 		file_type_img_ids.append(f['img_id'])
+		all_results.add(f['img_id'])
 	for k in keyword_query:
 		keyword_img_ids.append(k['img_id'])
+		all_results.add(k['img_id'])
 	
 	# narrow down
-	results = []
-	for i in rating_img_ids:
-		if len(file_type_img_ids)== 0 or i in file_type_img_ids:
-			if len(price_img_ids)== 0 or i in price_img_ids:
-				if len(stock_img_ids)== 0 or i in stock_img_ids:
-					if len(keyword_img_ids)== 0 or i in keyword_img_ids:
-						results.append(i)
+	narrowed = []
+	print(rating_img_ids, file_type_img_ids, price_img_ids, stock_img_ids, keyword_img_ids) # for checking
+	for i in all_results:
+		if len(keyword_img_ids) == 0 or i in keyword_img_ids:
+			if len(file_type_img_ids) == 0 or i in file_type_img_ids:
+				if len(price_img_ids) == 0 or i in price_img_ids:
+					if len(stock_img_ids) == 0 or i in stock_img_ids:
+						if len(rating_img_ids) == 0 or i in rating_img_ids:
+							narrowed.append(i)
 
-	print("You have " + str(len(results)) + " results.")
-	for img_id in results:
+	print("\nYou have " + str(len(narrowed)) + " result(s).")
+	for img_id in narrowed:
 		print(img_id)
 
 	take_input()
 
 
 def take_input():
-	img_id = input("Provide img_id if you wish to search by image.\nType 'q' if you wish to quit\nOtherwise, hit Enter for more search options: ")
+	to_do = input("Provide img_id if you wish to search by image.\nType 'q' if you wish to quit\nOtherwise, hit Enter for more search options: ")
+	if to_do == "q":
+		exit()
+	elif to_do == "":
+		# a separate field because for security there should be a limit on the file types that can be uploaded
+		# so a checkbox interface is likely a reasonable expectation and the flags for those checkboxes can be fed into here
+		file_type = input("Prefered image file type(s), space-separated: ").split()
+		# if none is provided, all types will be shown
+
+		price = input("Maximum price: ")
+		# if none is provided, all prices will be shown
+		try:
+			price = float(price)
+		except ValueError:
+			price = ""
+
+		# similar to price range filter (except will output results gte instead of lte)
+		rating = input("Prefered rating: (1 --> 5) ")
+		# if none is provided, all ratings will be shown
+		try:
+			rating = int(rating)
+			if (rating < 1 and rating > 5):
+				rating = 1
+		except:
+			rating = 1
+		
+		# assumes users search like on any search engine, with spaces between words
+		keywords = input("Please input your search terms: ")
+		# if none is provided all images will be shown
+
+		in_stock = input("Must the item you're looking for be in stock? (y/n) ")
+
+		search(in_stock, price, rating, file_type, keywords)
+		return
+	
 	# assume users can only search by an image that's in the database to find similar images
 	# when searching by an image, all that is needed is the image id, the rest can be retrieved from there
-	if img_id == "q":
-		exit()
-	elif img_id != "":
-		search_by_image(img_id)
-		return
-
-	# a separate field because for security there should be a limit on the file types that can be uploaded
-	# so a checkbox interface is likely a reasonable expectation and the flags for those checkboxes can be fed into here
-	file_type = input("Prefered image file type(s), space-separated: ").split()
-	# if none is provided, all types will be shown
-
-	price = input("Maximum price: ")
-	# if none is provided, all prices will be shown
-
-	# similar to price range filter (except will output results gte instead of lte)
-	rating = input("Prefered rating: (1 --> 5) ")
-	# if none is provided, all ratings will be shown
-	if rating=="" or (int(rating) < 1 and int(rating) > 5):
-		rating = 1
-	
-	# assumes users search like on any search engine, with spaces between words
-	keywords = input("Please input your search terms: ")
-	# if none is provided all images will be shown
-
-	in_stock = input("Must the item you're looking for be in stock? (y/n) ")
-
-	search(in_stock, price, rating, file_type, keywords)
-	return
+	try:
+		search_by_image(int(to_do))
+		take_input()
+	except:
+		take_input()
 
 
 def main():
